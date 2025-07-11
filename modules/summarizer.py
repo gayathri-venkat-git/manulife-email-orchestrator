979e8ab11d394086608ca0fb94acf0d7298dd185
gayathri-venkat-git <gayathri.vp@outlook.com>
2025-07-11 02:10:16 -0400

    updating rule based summarization

--- modules/summarizer.py
+++ modules/summarizer.py
@@ -5,3 +5,6 @@ summarizer = pipeline("summarization", model="t5-small")
 
 def summarize_with_llm(text):
     return summarizer(text[:1000])[0]['summary_text']
+def rule_based_summary(text):
+    lines = text.strip().split("\n")
+    return " ".join(lines[:3]) if lines else te
