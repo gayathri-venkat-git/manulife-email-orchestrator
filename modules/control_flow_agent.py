5757692ad56d2ea692e70f5899c971e3b907ed8e
gayathri-venkat-git <gayathri.vp@outlook.com>
2025-07-14 10:42:26 -0400

    introducing langgraph agent

--- modules/control_flow_agent.py
+++ modules/control_flow_agent.py
@@ -1,19 +1,9 @@
+from langgraph_refactored import build_langgraph_orchestrator
 from utils.helpers import detect_format
 from modules.email_parser import parse_email
-from modules.summarizer import summarize_with_llm, rule_based_summary
-from modules.priority_detector import classify_priority
 
 def orchestrate_email(raw_content):
     email_text = parse_email(raw_content, detect_format(raw_content))
-    
-    if len(email_text) < 200:
-        summary = rule_based_summary(email_text)
-    else:
-        summary = summarize_with_llm(email_text)
-
-    priority = classify_priority(email_text)
-
-    return {
-        "summary": summary,
-        "priority": priority
-    }
+    graph = build_langgraph_orchestrator()
+    result = graph.invoke({"text": email_text})
+    return result
